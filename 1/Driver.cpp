544eb91825984f4fa38f7b9f03896943a9f1973f
amir <amd.gharibipour@outlook.com>
2023-09-18 10:08:31 -0700

    driver class added

---
+++ 1/Driver.cpp
@@ -0,0 +1,19 @@
+#include "./Driver.hpp"
+
+
+Driver::Driver(std::string fileName){
+    this -> fileAddress = fileName;
+}
+
+std::vector<TA> Driver::readFile(){
+
+
+
+}
+
+bool Driver::updateFile(std::vector<TA> data){
+
+    return true;
+}
+
+bool closeFile;
--- 1/Driver.hpp
+++ 1/Driver.hpp
@@ -1,8 +1,23 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include "./TA.hpp"
+
 class Driver
 {
 private:
     /* data */
+
+    std::string fileAddress;
+    std::ifstream * db;
+    
+
 public:
-    Driver(/* args */);
+    Driver(std::string fileName);
+
+    std::vector<TA> readFile();
+    bool updateFile(std::vector<TA> data);
+    bool closeFile();
+
     ~Driver();
 };
